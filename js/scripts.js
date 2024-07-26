7710169f67525e61394570ea7e17e189897dec17
tech-noe <ktc24a31g0017@edu.kyoto-tech.ac.jp>
2024-07-26 10:12:41 +0900

    tabmenu

--- js/scripts.js
+++ js/scripts.js
@@ -63,6 +63,27 @@ $(document).ready(function() {
     }
 });
 
+// 世界遺産ページのタブメニュー
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tab-link');
+    const panes = document.querySelectorAll('.tab-pane');
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', () => {
+            const target = document.getElementById(tab.getAttribute('data-tab'));
+
+            // すべてのタブとコンテンツから 'active' クラスを削除
+            tabs.forEach(t => t.classList.remove('active'));
+            panes.forEach(p => p.classList.remove('active'));
+            
+            // クリックされたタブとその対応するコンテンツに 'active' クラスを追加
+            tab.classList.add('active');
+            target.classList.add('active');
+        });
+    });
+});
+
+
 
 
   
